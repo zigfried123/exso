5ee986d525e6dbd31d3c6ece729c98760540eb51
zigfried123 <zigfried123@mail.ru>
2024-05-29 22:03:24 +0400

    fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <title>{{ $title ?? 'Todo Manager' }}</title>
 </head>
 <body>
-<h1>Exso</h1>
+<h1>Ekso</h1>
 <a href="/">Главная</a> <a href="/jobs">Мы предлагаем (виды работ)</a> <a href="/examples">Примеры наших работ</a> <a href="/contacts">Контакты</a>
 <hr/>
 {{ $slot }}
